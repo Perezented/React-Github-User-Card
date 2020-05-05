011fa78a9de2973ae521b411ae18689f078be57d
Michael Perez <mperez6473@gmail.com>
2020-05-05 15:17:07 -0500

    Have axios running to get My info and just added an axios to get followers info array. Have my card displaying some of my information now.

--- usercards/src/App.js
+++ usercards/src/App.js
@@ -2,19 +2,37 @@ import React from 'react';
 import MyCard from './components/MyCard';
 import './App.css';
 
+import axios from 'axios';
+
 export default class App extends React.Component {
     state = {
         MyData: [],
-        FollowersData: [],
+        FollowersArray: [],
     };
 
-    componentDidMount() {}
+    componentDidMount() {
+        axios
+            .get('https://api.github.com/users/Perezented')
+            .then((res) => {
+                console.log('ratelimit left', res.headers);
+                this.setState({ MyData: res.data });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        axios
+            .get('https://api.github.com/users/Perezented/followers')
+            .then((res) => {
+                console.log('Followers', res.data);
+                this.setState({ FollowersArray: res.data });
+            });
+    }
 
     render() {
         return (
             <div className="App">
                 <h1>Github User Cards in React</h1>
-                <MyCard />
+                <MyCard myData={this.state.MyData} />
             </div>
         );
     }
--- usercards/src/components/MyCard.jsx
+++ usercards/src/components/MyCard.jsx
@@ -4,6 +4,21 @@ import axios from 'axios';
 
 export default class MyCard extends React.Component {
     render() {
-        return <section>My Card will go here</section>;
+        console.log(this.props.myData);
+        const myStuff = this.props.myData;
+        return (
+            <section>
+                <h2>{this.props.myData.login}</h2>
+                <h3>{this.props.myData.name}</h3>
+                <p>{myStuff.location}</p>
+                <p>{myStuff.bio}</p>
+                <div>
+                    <h5>Total Public Repos: {myStuff.public_repos}</h5>
+                    <h5>Total Followers: {myStuff.followers}</h5>
+                    <h5>Total Following: {myStuff.following}</h5>
+                </div>
+                <h6>GitHub provided this info here: {myStuff.url}</h6>
+            </section>
+        );
     }
 }
